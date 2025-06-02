95bb229c821064067699af03e30f9fc185cedb15
Taishimasu15 <jorgearizaw10@gmail.com>
2025-06-02 11:35:14 -0500

    Nuevo commit y nuevos cambios

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from flask import Flask 
+from flask_cors import CORS 
 
 app = Flask (__name__)
+CORS(app)
+
 
 @app.route("/")
 def hello_world(): 
-    return "Hello worl"
+    return "te amo mi amor, me quiero casar contigo"
+
+@app.route("/api/users")
+def get_users ():
+    return{
+        "users" : ["Alice", "Bob", "Charlie"]
+    }
+
+@app.route("/api/fruits")
+def get_fruits ():
+    return{
+        "fruits" : ["Cambur", "Cereza", "Uvita"]
+    }
 
 if __name__ == "__main__":
     app.run(debug=True) #by default, Flask runs on port 5000
