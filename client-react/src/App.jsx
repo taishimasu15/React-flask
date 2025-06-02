c95787720e3e71f26b90dc7559d29ed728bacafd
Taishimasu15 <jorgearizaw10@gmail.com>
2025-06-02 18:47:02 -0500

    Commit para todo nuevo

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/users")
+      .then ((res) => res.json)
+      .then ((data) => setUsers(data))
+  })
 
   return (
     <>
